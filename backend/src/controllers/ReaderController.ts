66c91a149d3bd46b1835e61903fc08c920d5b43a
Sancal0606 <carloz1.2.sanchez@gmail.com>
2025-02-11 09:23:22 -0600

    Login back end

--- backend/src/controllers/ReaderController.ts
+++ backend/src/controllers/ReaderController.ts
@@ -1,34 +1,78 @@
 import { Request, Response } from "express";
 import AbstractController from "./AbstractController";
-import db from "../models"
+import db from "../models";
 
-class ReaderController extends AbstractController{
-    private static _instance: ReaderController;
-    public static get instance(): ReaderController{
-        if(this._instance){
-            return this._instance;
-        }
-        this._instance = new ReaderController("reader");
-        return this._instance;
+class ReaderController extends AbstractController {
+  private static _instance: ReaderController;
+  public static get instance(): ReaderController {
+    if (this._instance) {
+      return this._instance;
     }
+    this._instance = new ReaderController("reader");
+    return this._instance;
+  }
 
-    protected initializeRoutes(): void {
-        this.router.post("/assign",this.postAssignBook.bind(this));
+  protected initializeRoutes(): void {
+    this.router.post("/assign", this.postAssignBook.bind(this));
+    this.router.post("/user",this.postUser.bind(this));
+    this.router.get("/login",this.getLogIn.bind(this));
+  }
+
+  private async postAssignBook(req: Request, res: Response) {
+    console.log(req.body);
+    try {
+      const asign = await db["bookReader"].create(req.body);
+      console.log("Assign Success");
+      res.status(200).json("Assign Success");
+    } catch (err) {
+      console.log("error");
+      console.log(err);
+      res.status(500).send("Error on Reader controller");
     }
+  }
 
-    private async postAssignBook(req: Request, res:Response){
-        console.log(req.body);
-        try{
-            const asign = await db["bookReader"].create(req.body);
-            console.log("Assign Success");
-            res.status(200).json("Assign Success");
-        }catch(err){
-            console.log("error");
-            console.log(err);
-            res.status(500).send("Error on Reader controller")
-        }
+  private async postUser(req: Request, res: Response) {
+    console.log(req.body);
+    try {
+      const assign = await db["reader"].create(req.body);
+      res.status(200).json("User Assign Success");
+      console.log("Usuario creado");
+    } catch (err) {
+      console.log("error");
+      console.log(err);
+      res.status(500).send("Error on Reader controller");
     }
+  }
 
+  private async getLogIn(req: Request, res: Response) {
+    try {
+      const { Op } = require("sequelize");
+
+      const input_mail = req.query.mail;
+      const input_password = req.query.password;
+
+      const finalUser = await db["reader"].findOne({
+        where: {
+          [Op.and]: [
+            { mail: input_mail },
+            { password: input_password },
+          ],
+        },
+      });
+
+      if (finalUser === null) {
+        console.log("User not found");
+        res.status(500).json("User not found");
+      } else {
+        console.log("User found");
+        res.status(200).json("User found");
+      }
+    } catch (err) {
+      console.log("error");
+      console.log(err);
+      res.status(500).send("Error on Reader controller");
+    }
+  }
 }
 
 export default ReaderController;
--- bookate-app/src/components/LoginSignUp/LoginSignUp.jsx
+++ bookate-app/src/components/LoginSignUp/LoginSignUp.jsx
@@ -1,14 +1,55 @@
 import React, { act, useState } from "react";
 import styles from "./LoginSignUp.module.css";
 import { getImageUrl } from "../../utils";
+import { v4 as uuidv4 } from "uuid";
+import { header } from "motion/react-client";
 
-const LoginSignUp = ({setLogIn}) => {
+const LoginSignUp = ({ setLogIn }) => {
   const [action, setAction] = useState("Login");
 
-    const handleSubmit = () =>
-    {
-        setLogIn(true);
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const modifyName = (event) => {
+    setName(event.target.value);
+  };
+
+  const modifyMail = (event) => {
+    setMail(event.target.value);
+  };
+
+  const modifyPassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const optionsPost = {
+    method: "POST",
+    header: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      name: "TestName",
+      mail: "TestMail",
+      password: "TestPassword",
+    }),
+  };
+
+  const handleSubmit = () => {
+    if (action === "Sign Up") {
+      console.log(optionsPost.body);
+    
+      fetch("http://localhost:8080/reader/user", optionsPost)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => console.log(error));
+    } else {
+      //fetch(`/assets/${path}`)
     }
+    setLogIn(true);
+  };
 
   return (
     <section className={styles.container} id="login">
@@ -22,17 +63,21 @@ const LoginSignUp = ({setLogIn}) => {
         ) : (
           <div className={styles.input}>
             <img src={getImageUrl("person.png")} alt=""></img>
-            <input type="text" placeholder="Name"></input>
+            <input type="text" placeholder="Name" onChange={modifyName}></input>
           </div>
         )}
 
         <div className={styles.input}>
           <img src={getImageUrl("email.png")} alt=""></img>
-          <input type="email" placeholder="Email"></input>
+          <input type="email" placeholder="Email" onChange={modifyMail}></input>
         </div>
         <div className={styles.input}>
           <img src={getImageUrl("password.png")} alt=""></img>
-          <input type="password" placeholder="Password"></input>
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={modifyPassword}
+          ></input>
         </div>
       </div>
       {action === "Login" ? (
